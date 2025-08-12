a5127c7c06d94ca910cf081c52535d5450505004
yagen <yagen1111@gmail.com>
2025-08-12 17:50:36 +0300

    fixfiox

--- infra-modules/kube-addon/cluster-autoscaler.tf
+++ infra-modules/kube-addon/cluster-autoscaler.tf
@@ -1,94 +1,93 @@
-data "aws_iam_openid_connect_provider" "connect" {
+data "aws_iam_openid_connect_provider" "this" {
   arn = var.openid_provider_arn
 }
 
 data "aws_iam_policy_document" "cluster_autoscaler" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect = "Allow"
-  condition {
-    test     = "StringEquals"
-    variable = "${replace(data.aws_iam_openid_connect_provider.connect.url, "https://", "")}:sub"
-    values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
-  }
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_iam_openid_connect_provider.this.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
+    }
 
     principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+      identifiers = [data.aws_iam_openid_connect_provider.this.arn]
+      type        = "Federated"
     }
   }
 }
 
 resource "aws_iam_role" "cluster_autoscaler" {
-  count= var.enable_cluster_autoscaler ? 1 : 0
+  count = var.enable_cluster_autoscaler ? 1 : 0
 
-  name               = "${var.eks_name}-cluster-autoscaler"
   assume_role_policy = data.aws_iam_policy_document.cluster_autoscaler.json
+  name               = "${var.eks_name}-cluster-autoscaler"
 }
 
 resource "aws_iam_policy" "cluster_autoscaler" {
-    count = var.enable_cluster_autoscaler ? 1 : 0
-    name = "${var.eks_name}-cluster-autoscaler"
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Effect = "Allow"
-                Action = [
-                    "autoscaling:DescribeAutoScalingGroups",
-                    "autoscaling:DescribeAutoScalingInstances",
-                    "autoscaling:DescribeLaunchConfigurations",
-                    "autoscaling:DescribeAutoScalingActivities",
-                    "ec2:DescribeInstancesTypes",
-                    "ec2:DescribeTemplateVersions"
-                ]
-                Effect = "Allow"
-                Resource = "*"
-            },
-            {
-                Effect = "Allow"
-                Action = [
-                    "autoscaling:SetDesiredCapacity",
-                    "autoscaling:TerminateInstanceInAutoScalingGroup"
-                ]
-                Effect = "Allow"
-                Resource = "*"
-            }
-        ]
-    })
-}
-resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
-    count = var.enable_cluster_autoscaler ? 1 : 0
+  count = var.enable_cluster_autoscaler ? 1 : 0
 
-    role       = aws_iam_role.cluster_autoscaler[0].name
-    policy_arn = aws_iam_policy.cluster_autoscaler[0].arn
+  name = "${var.eks_name}-cluster-autoscaler"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeScalingActivities",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeLaunchTemplateVersions"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  count = var.enable_cluster_autoscaler ? 1 : 0
+
+  role       = aws_iam_role.cluster_autoscaler[0].name
+  policy_arn = aws_iam_policy.cluster_autoscaler[0].arn
 }
 
 resource "helm_release" "cluster_autoscaler" {
-    count = var.enable_cluster_autoscaler ? 1 : 0
+  count = var.enable_cluster_autoscaler ? 1 : 0
 
-    name       = "autoscaler"
-    repository = "https://kubernetes.github.io/autoscaler"
-    chart      = "cluster-autoscaler"
-    version    = var.cluster_autoscaler_helm_version
-    namespace  = "kube-system"
+  name = "autoscaler"
 
-    set = [
-        {
-            name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-            value = aws_iam_role.cluster_autoscaler[0].arn
-        },
-        {
-            name  = "autoDiscovery.clusterName"
-            value = var.eks_name
-        },
-        {
-            name  = "rbac.serviceAccount.name"
-            value = "cluster-autoscaler"
-        }
-    ]
+  repository = "https://kubernetes.github.io/autoscaler"
+  chart      = "cluster-autoscaler"
+  namespace  = "kube-system"
+  version    = var.cluster_autoscaler_helm_version
 
-    depends_on = [
-        aws_iam_role_policy_attachment.cluster_autoscaler
-    ]
+  set  = [
+    {
+      name  = "rbac.serviceAccount.name"
+      value = "cluster-autoscaler"
+    },
+    {
+      name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = aws_iam_role.cluster_autoscaler[0].arn
+    },
+    {
+      name  = "autoDiscovery.clusterName"
+      value = var.eks_name
+    }
+  ]
 }
--- infra-v4/dev/kube-addons/terragrunt.hcl
+++ infra-v4/dev/kube-addons/terragrunt.hcl
@@ -14,14 +14,14 @@ inputs = {
   env = include.env.locals.env
   cluster_autoscaler_helm_version = "9.28.0"
   eks_name = dependency.eks.outputs.eks_name
-  marge_strategy = "no_marge"
   openid_provider_arn = dependency.eks.outputs.openid_connect_provider_arn
+  enable_cluster_autoscaler      = true
 }
 
 dependency "eks" {
     config_path = "../eks"
     mock_outputs = {
-        eks_name = "eks-cluster"
+        eks_name = "dev-eks"
         openid_connect_provider_arn = "arn:aws:iam::123456789012:oidc-provider"
     }
 }
@@ -39,13 +39,9 @@ data "aws_eks_cluster_auth" "eks" {
 
 provider "helm" {
   kubernetes = {
-    host = data.aws_eks_cluster.eks.endpoint
+    host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks.name]
-      command = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
 EOF
